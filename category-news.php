4c7be5b2aa503220b5336cdff24fdb68f7b5bd19
Ryan Weeks <ryanweeks@Hanna-Oakes.local>
2017-05-09 10:35:01 +0100

    Category colour change

---
+++ category-news.php
@@ -0,0 +1,48 @@
+<?php
+/*Template Name : Nature */
+?>
+<?php get_header();?>
+<div id="content">
+
+
+
+
+
+
+    <h1>News</h1>
+<?php wp_pagenavi(); ?>
+<?php
+
+if ( have_posts() ) :
+        while ( have_posts() ) : the_post();
+    ?>
+    <div class="post_item post_item_news">
+        <div id="posthead2">
+            </br>
+            <a href="<?php the_permalink(); ?>"><?php the_title();?></a>
+        </div>
+        <div id="posthead3">
+            </br>
+            <a href="<?php the_permalink(); ?>"><?php the_excerpt(); ?></a>
+            </br>
+            <!-- </a> -->
+        </div>
+
+    </div>
+    <?php
+        endwhile;
+    endif;
+    ?>
+    </div>
+    <div id="navigation">
+    <?php wp_pagenavi(); ?>
+
+    </div>
+
+
+    </div>
+
+
+    <div id="delimiter">
+    </div>
+    <?php get_footer();?>
